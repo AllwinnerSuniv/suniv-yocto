8ee10a49eb725c5b70da457d746662d98dfac83c
iotah <writeforever@foxmail.com>
2023-04-24 18:38:10 +0800

    add build script
    Signed-off-by: iotah <writeforever@foxmail.com>

---
+++ build.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# global variables
+export NCPU=$(grep -c processor /proc/cpuinfo)
+export CWD=$(pwd)
+export RED_COLOR='\E[1;31m'  #红
+export GREEN_COLOR='\E[1;32m' #绿
+export YELOW_COLOR='\E[1;33m' #黄
+export BLUE_COLOR='\E[1;34m'  #蓝
+export PINK='\E[1;35m'      #粉红
+export RES='\E[0m'
+
+GIT=https://github.com
+REMOTE=AllwinnerSuniv
+UPSTREAM=${GIT}/${REMOTE}
+RELEASE=kirkstone
+
+START_BUILD_TIME=`date +%H:%M:%S`
+LOGFILE=yocto_build_${START_BUILD_TIME}.log
+
+function echo_warning() {
+        echo -e "${YELOW_COLOR} [ `date +%H:%M:%S` ]: $1 ${RES}"
+}
+export -f echo_warning
+
+function echo_debug() {
+        echo -e "${BLUE_COLOR} [ `date +%H:%M:%S` ]: $1 ${RES}"
+}
+export -f echo_debug
+
+function echo_error() {
+        echo -e "${RED_COLOR} [ `date +%H:%M:%S` ]: $1 ${RES}"
+}
+export -f echo_error
+
+function echo_success() {
+        echo -e "${GREEN_COLOR} [ `date +%H:%M:%S` ]: $1 ${RES}"
+}
+export -f echo_success
+
+# $1 repo
+function do_fetch() {
+	echo $1
+    if [ ! -d "$1/" ]; then
+        echo_debug "can't find $1, pulling ..."
+        git clone -q --depth 1 -b $RELEASE ${UPSTREAM}/$1
+    else
+        echo_debug "$1/ was detected, make sure it's okay."
+    fi
+}
+
+# check if a root user running this script
+if [ "$(whoami)" = "root" ]; then
+        echo_error "Should never run this script as root. Exiting..."
+        exit -1
+fi
+
+# build the castle
+BSPDIR=$CWD
+# get into the real sources directory
+if [ ! -d ${BSPDIR}/yocto ]; then
+	mkdir -p ${BSPDIR}/yocto
+fi
+
+cd ${BSPDIR}/yocto
+
+do_fetch poky
+
+do_fetch meta-openembedded
+
+# pull layers we defined
+
+# is there any patchs need to apply?
+
+
